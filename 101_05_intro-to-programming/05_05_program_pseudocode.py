913c66644abd1532d8cba9d3967a552957a45677
martin-martin <breuss.martin@gmail.com>
2021-04-02 17:04:37 +0200

    Add pseudocode lab

---
+++ 101_05_intro-to-programming/05_05_program_pseudocode.py
@@ -0,0 +1,8 @@
+# Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+# Break down some of the steps you can think of and write them in
+# this file as pseudocode.
+# You can come back and update this file when you've learned more.
+# You can also add real Python code after learning the necessary concepts.
+# For now, just practice breaking larger tasks into smaller steps
+# and writing out your thoughts in pseudocode.
