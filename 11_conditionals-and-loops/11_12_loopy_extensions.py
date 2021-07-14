025e5a631a119e51b62baba199c8fc062099dcdf
Martin Breuss <martin-martin@users.noreply.github.com>
2021-07-14 08:27:20 +0200

    Tighten up instructions

--- 11_conditionals-and-loops/11_12_loopy_extensions.py
+++ 11_conditionals-and-loops/11_12_loopy_extensions.py
@@ -1,5 +1,6 @@
 # Proof that the following file is a .pdf file using a `for` loop.
 # - Don't use the string method you've used to solve this before!
+# - Don't use the `in` keyword to look for a sub-string!
 # - Don't use any string slicing technique either!
 #
 # You'll see that it'll be tricky to solve this challenge with a loop :)
